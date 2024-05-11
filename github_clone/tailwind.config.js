502e2018afbae142fd86735209738b4a35d28aed
Desire <lfs451@gmail.com>
2024-05-11 11:57:15 +0800

    完成侧边栏样式，并使用函数生成百分比宽度样式

--- github_clone/tailwind.config.js
+++ github_clone/tailwind.config.js
@@ -1,9 +1,22 @@
 /** @type {import('tailwindcss').Config} */
+
+const generateWidthClasses = () => {
+  const widthClasses = {};
+  for (let i = 1; i <= 100; i++) {
+    widthClasses[`${i}/100`] = `${i}%`;
+  }
+  return widthClasses;
+};
+
+
 module.exports = {
   content: ["src/**/*.{js,jsx,ts,tsx,html}"],
   theme: {
-    extend: {},
+    extend: {
+      width: generateWidthClasses(),
+    },
   },
   plugins: [],
 }
+
 
